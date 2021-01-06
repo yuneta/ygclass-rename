fd5d2b484fb79050df0e5a19a35cb4731d645739
Niyamaka <niyamaka@yuneta.io>
2021-01-06 11:52:59 +0100

    v4.6.0

--- ygclass_rename.c
+++ ygclass_rename.c
@@ -16,7 +16,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "ygclass-rename"
-#define APP_VERSION     "4.5.0"
+#define APP_VERSION     "4.6.0"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
