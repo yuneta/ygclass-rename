e0845d3972ee6aae720071d0903eaa341458ec04
Niyamaka <niyamaka@yuneta.io>
2021-01-17 16:45:23 +0100

    v4.6.9 c_authz liboauth2 - It seems required to used cache in liboauth2

--- ygclass_rename.c
+++ ygclass_rename.c
@@ -16,7 +16,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "ygclass-rename"
-#define APP_VERSION     "4.6.8"
+#define APP_VERSION     "4.6.9"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
