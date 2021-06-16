568773facacdce09b9ed2fcd893e442de3525b72
Niyamaka <niyamaka@yuneta.io>
2021-06-16 19:24:42 +0200

    v4.14.0 A new feature: node owner, the owner of a (prod/staging/test/dev) node governed by a yuneta agent, WARNING gobj_yuno_factory() api has changed

--- ygclass_rename.c
+++ ygclass_rename.c
@@ -16,7 +16,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "ygclass-rename"
-#define APP_VERSION     "4.13.3"
+#define APP_VERSION     "4.14.0"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
