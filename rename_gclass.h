9dc6cb2ac3cec149c7bcc4ed3c3940c6a575e8ae
Niyamaka <niyamaka@yuneta.io>
2021-07-10 19:55:10 +0200

    pragma once

--- rename_gclass.h
+++ rename_gclass.h
@@ -1,6 +1,4 @@
-#ifndef _MAKE_SKELETON_H
-#define _MAKE_SKELETON_H 1
-
+#pragma once
 
 #ifdef __cplusplus
 extern "C"{
@@ -10,6 +8,4 @@ int rename_gclass(char *old_gclass_name, char *new_gclass_name);
 
 #ifdef __cplusplus
 }
-#endif
-
 #endif
