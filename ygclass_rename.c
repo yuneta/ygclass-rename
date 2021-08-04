ee93eba4356f20b4404b4bfebe536c8843fe67fc
Niyamaka <niyamaka@yuneta.io>
2021-08-04 08:13:15 +0200

    v4.15.8 Add ON_ID_NAK event

--- ygclass_rename.c
+++ ygclass_rename.c
@@ -16,7 +16,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "ygclass-rename"
-#define APP_VERSION     "4.15.7"
+#define APP_VERSION     "4.15.8"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
