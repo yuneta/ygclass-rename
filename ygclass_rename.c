bc8839e23b91269e1381fc21d371608284dec018
Niyamaka <niyamaka@yuneta.io>
2021-09-09 13:27:03 +0200

    v4.17.0 WARNING Need recompila, external-libs: upgraded to pcre2-10.37 and OpenSSL_1_1_1l, fix problem in Task gclass

--- ygclass_rename.c
+++ ygclass_rename.c
@@ -16,7 +16,7 @@
  *      Constants
  ***************************************************************************/
 #define NAME            "ygclass-rename"
-#define APP_VERSION     "4.16.1"
+#define APP_VERSION     "4.17.0"
 #define APP_DATETIME    __DATE__ " " __TIME__
 #define APP_SUPPORT     "<niyamaka at yuneta.io>"
 
